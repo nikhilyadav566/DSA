fc09f3df337b1b2e6096abf65b395776bc6d2d5e
nikhilyadav566 <nikhilyad42004@gmail.com>
2026-08-08 22:30:54 +0530

    first commit

---
+++ Basics_1/02_main.cpp
@@ -0,0 +1,12 @@
+// We can also write the program like this.
+
+#include<iostream>
+using namespace std;  // Allows us to use cout, cin, endl, etc., without writing std::
+
+int main() {
+    cout << "Hello India" << endl;  // Prints text and moves to the next line.
+    cout << '\n';                   // Prints another new line.
+    cout << "Hii";                  // Prints "Hii".
+}
+
+// This style is commonly used by beginners for shorter code.
---
+++ Basics_1/03_takingInput.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {                          // Main function: Program starts here.
+    cout << "Enter your number" << endl;   // Displays a message asking for input.
+    int a;                            // Declares an integer variable to store the input.
+    cin >> a;                         // Takes input from the user and stores it in 'a'.
+    cout << "You entered " << a << endl;   // Displays the entered number.
+    return 0;                         // Ends the program successfully.
+}
